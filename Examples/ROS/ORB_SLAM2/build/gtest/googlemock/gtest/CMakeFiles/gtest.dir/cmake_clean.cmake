822206138c78737f788b2a5770ff9d0c6c62a655
gychen-n <1638130844@qq.com>
2021-01-26 13:10:46 +0800

    first commit

---
+++ Examples/ROS/ORB_SLAM2/build/gtest/googlemock/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/gyc/ORB-SLAM/src/orb-slam2-save-map/ORB_SLAM2/Examples/ROS/ORB_SLAM2/lib/libgtest.pdb"
+  "/home/gyc/ORB-SLAM/src/orb-slam2-save-map/ORB_SLAM2/Examples/ROS/ORB_SLAM2/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mono_euroc.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gyc/ORB-SLAM/src/orb-slam2-save-map/ORB_SLAM2/Examples/Monocular/mono_euroc.cc" "/home/gyc/ORB-SLAM/src/orb-slam2-save-map/ORB_SLAM2/build/CMakeFiles/mono_euroc.dir/Examples/Monocular/mono_euroc.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/gyc/relay/Pangolin/include"
+  "/home/gyc/relay/Pangolin/build/src/include"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gyc/ORB-SLAM/src/orb-slam2-save-map/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
